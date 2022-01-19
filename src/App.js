b263c5eb2c6862621003c09256bfc601e9459df4
Guru Pawar <62001513+gurupawar@users.noreply.github.com>
2022-01-19 15:47:15 +0530

    ✨

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ function App() {
             </label>
             <label>usd</label>
           </div>
+          {/* ============================ Big Input Box  */}
           <InputBox />
+          {/* ============================ Default Select Value  */}
           <div className="default__value__conversion">
             <div className="default-value">
               <span>$1000</span>
@@ -26,10 +28,46 @@ function App() {
             </div>
             <div className="conversionTo__cake">~ cake 0.000</div>
           </div>
+
+          {/* ============================ Time frame  */}
           <div className="timeFrame">
-            <spna className="frameBG">
+            <div className="frameBG">
               <h4 className="title">Timeframe</h4>
-            </spna>
+              <div className="time">
+                <span className="time-selected">1 Day</span>
+                <span>7 Day</span>
+                <span>30 Day</span>
+                <span>1 Year</span>
+                <span>5 Year</span>
+              </div>
+            </div>
+          </div>
+          {/* ============================ Accelerated APY  */}
+          <div className="apy__toggle">
+            <span>Enable Accelerated APY</span>
+            <label class="switch">
+              <input type="checkbox" />
+              <span class="slider round checked"></span>
+            </label>
+          </div>
+          {/* ============================ Select Tier  */}
+
+          <div className="select__tier">
+            <h4 className="title gray">Select Tier</h4>
+            <div className="time">
+              <span className="time-selected">1 Day</span>
+              <span>7 Day</span>
+              <span>30 Day</span>
+              <span>1 Year</span>
+              <span>5 Year</span>
+            </div>
+          </div>
+
+          {/* ============================ ROI at Current Rates  */}
+          <div className="current__rate">
+            <h4 className="title right">ROI at Current Rates</h4>
+            <p className="current__rate-result">0.00 USD</p>
+            <span className="conversion">~ 0.000 CAKE + 0.000000 DON</span>
           </div>
         </div>
       </div>
