c72d36b43cb7886494845d34980dc3dabb83ed81
Guru Pawar <62001513+gurupawar@users.noreply.github.com>
2022-01-19 14:27:31 +0530

    ✨

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import cake from "./assets/cake-logo.svg";
+import InputBox from "./components/InputBox";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="card">
+          <h2>ROI Calculator</h2>
+          <div className="switch__wrapper">
+            <label>
+              <img src={cake} alt="" />
+              cake
+            </label>
+            <label class="switch">
+              <input type="checkbox" />
+              <span class="slider round checked"></span>
+            </label>
+            <label>usd</label>
+          </div>
+          <InputBox />
+          <div className="default__value__conversion">
+            <div className="default-value">
+              <span>$1000</span>
+              <span>$100</span>
+            </div>
+            <div className="conversionTo__cake">~ cake 0.000</div>
+          </div>
+          <div className="timeFrame">
+            <spna className="frameBG">
+              <h4 className="title">Timeframe</h4>
+            </spna>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/InputBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InputBox = () => {
+  return (
+    <div className="input__box">
+      <input type="number" placeholder="0.000 USD" />
+    </div>
+  );
+};
+
+export default InputBox;
